0a11c2dda56f0ab3b58d06fb2007020a5bfbd95b
Mikko Kotila <mailme@mikkokotila.com>
2025-03-09 13:15:16 -0400

    chore: Cleanup commit()

--- app/routes/commit.py
+++ app/routes/commit.py
@@ -3,12 +3,15 @@ def commit(self):
     import subprocess
 
     try:
+        
         subprocess.run(["git", "add", "data/" + self.filename], check=True)
         subprocess.run(["git", "commit", "-m", "Autosave commit"], check=True)
         subprocess.run(["git", "push", "origin", "main"], check=True)
+        
         print("Changes committed and pushed to GitHub successfully.")
     
     except subprocess.CalledProcessError as e:
+        
         print(f"An error occurred while committing to GitHub: {e}")
 
     return '', 204
